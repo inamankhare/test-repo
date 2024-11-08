70be2a30a1878df3cccf136a2de104629f4e987f
inamankhare <inamankhare@gmail.com>
2024-11-08 11:33:38 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,98 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const[formdata,setformdata] = useState({
+    name:"",surname:"",email:""
+  })
+
+  
+
+  
+
+function changeHandler(e){
+  setformdata(prevformdata=>{
+    return{
+      ...prevformdata,
+    [e.target.name]:e.target.value
+    }
+  });
+
+}
+
+function clearHandler(){
+  
+ setformdata({name:"",surname:"",email:""})
+
+}
+
+
+
+function submitHandler(e){
+  e.preventDefault();
+  console.log(formdata)
+
+  alert('Form Submitted')
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React form</h1>
+      <form className='form' onSubmit={submitHandler}>
+      <div className='form-inputs'>
+      <input 
+        type='text' 
+        placeholder='Your Name'
+        onChange={changeHandler}
+        name='name'
+        value={formdata.name}
+    />
+
+    <input
+
+    type='text'
+    name='surname'
+    onChange={changeHandler}
+    value={formdata.surname}
+    placeholder='surname'
+    
+    />
+
+<input
+
+type='email'
+name='email'
+onChange={changeHandler}
+value={formdata.email}
+placeholder='email'
+
+/>
+
+
+<div className='BTN'>
+<button className='stbtn' onSubmit={changeHandler}>
+  Submit
+</button>
+
+<button className='clrbtn' onClick={clearHandler}>
+  Clear
+</button>
+</div>
+     
     </div>
+
+    
+      
+
+
+      </form>
+
+      
+      </div>
+  
+
   );
 }
 
